5cb02068448096edfa504391f8068314a2235231
Steve Fallet <steve.fallet@divtec.ch>
2023-05-08 11:15:05 +0200

    1st

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="ESIG logo"
+      src="~assets/esig.png"
+      style="max-width: 400px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
